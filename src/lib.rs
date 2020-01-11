102969b922cd5aa5d0788d45c067921fa9d75ac6
Bjorn Martens <bjorn@expeditious.nl>
2020-01-11 13:39:06 +0100

    Made rust interface over cpp window abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,42 @@ pub struct Color
 }
 
 extern "C" {
-    pub fn window_create(buffer: *const Color, size: libc::size_t) -> *const libc::c_void;
-    pub fn window_destroy(cookie: *const libc::c_void);
-    pub fn window_pump(cookie: *const libc::c_void) -> bool;
-    pub fn window_update(cookie: *const libc::c_void);
+    fn window_create(buffer: *const Color, size: libc::size_t) -> *const libc::c_void;
+    fn window_destroy(handle: *const libc::c_void);
+    fn window_pump(handle: *const libc::c_void) -> bool;
+    fn window_update(handle: *const libc::c_void);
+}
+
+pub struct Window
+{
+    handle: *const libc::c_void
+}
+
+impl Window
+{
+    pub fn new(buffer: &[Color]) -> Window {
+        unsafe {
+            Window {
+                handle: window_create(buffer.as_ptr(), buffer.len() as libc::size_t)
+            }
+        }
+    }
+    pub fn update(&self) {
+        unsafe {
+            window_update(self.handle);
+        }
+    }
+    pub fn pump(&self) -> bool {
+        unsafe {
+            window_pump(self.handle)
+        }
+    }
+}
+
+impl Drop for Window {
+    fn drop(&mut self) {
+        unsafe {
+            window_destroy(self.handle);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 use software_renderer_rs::*;
 
 fn main() {
-    unsafe {
-        let window_size = 640 * 480;
-        let mut fb: Vec<Color> = Vec::with_capacity(window_size);
-        for _i in 0..window_size {
-            fb.push(Color { r: 255, g: 0, b: 0, a: 255});
-        }
+    let window_size = 640 * 480;
+    let mut fb: Vec<Color> = Vec::with_capacity(window_size);
+    for _i in 0..window_size {
+        fb.push(Color { r: 255, g: 0, b: 0, a: 255});
+    }
 
-        let context = window_create(fb.as_ptr(), fb.len() as libc::size_t);
-
-        let mut i = 0;
-        while window_pump(context) {
-            fb[i % window_size] = Color {r: 0, g: 0, b: 255, a: 255 };
-            window_update(context);
-            i += 1;
-        }
-        window_destroy(context);
+    let window: Window = Window::new(fb.as_slice());
+    let mut i = 0;
+    while window.pump() {
+        fb[i % window_size] = Color {r: 0, g: 0, b: 255, a: 255 };
+        window.update();
+        i += 1;
     }
 }
+
