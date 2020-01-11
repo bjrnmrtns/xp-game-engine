82f550d1a5b04e2f5c1b7ad34fb22809a1e96868
Bjorn Martens <bjorn@expeditious.nl>
2020-01-11 15:58:34 +0100

    Removed i var from loop

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ fn main() {
     let height: usize = 480;
     let mut canvas = Canvas::new(width, height, Color{r: 0, g:0, b: 255, a: 255});
     let window: Window = Window::new(&canvas);
-    let mut i: u32 = 0;
     while window.pump() {
-        canvas.set(10, 10, Color{r: 255 as u8, g: 0, b: 0, a: 255 });
+        canvas.set(10, 10, Color{r: 255, g: 0, b: 0, a: 255 });
         window.update();
-        i = i + 1;
     }
 }
