7db889835c9037e84627380d2fa3483847a9fbe9
Bjorn Martens <bjorn@expeditious.nl>
2020-02-12 19:30:40 +0100

    Move obj loading to separate function

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn barycentric(v0: &Vector2<i32>, v1: &Vector2<i32>, v2: &Vector2<i32>, p: Vecto
     return Vector3::new(1.0 - (u.x as f32 + u.y as f32) / u.z as f32, u.y as f32 / u.z as f32, u.x as f32 / u.z as f32);
 }
 
-fn draw_triangle(v0: Vector3<f32>, v1: Vector3<f32>, v2: Vector3<f32>, color: &Color, mut canvas: &mut Canvas, zbuffer: &mut Vec<f32>, width: usize, height: usize) {
+fn draw_triangle(shader: &Shader, v0: Vector3<f32>, v1: Vector3<f32>, v2: Vector3<f32>, color: &Color, mut canvas: &mut Canvas, zbuffer: &mut Vec<f32>, width: usize, height: usize) {
     let v0i: Vector2<i32> = Vector2::new(v0.x as i32, v0.y as i32);
     let v1i: Vector2<i32> = Vector2::new(v1.x as i32, v1.y as i32);
     let v2i: Vector2<i32> = Vector2::new(v2.x as i32, v2.y as i32);
@@ -82,7 +82,7 @@ fn draw_triangle(v0: Vector3<f32>, v1: Vector3<f32>, v2: Vector3<f32>, color: &C
         for y in y_min..y_max {
             let barycentric_screen = barycentric(&v0i, &v1i, &v2i, Vector2::new(x, y));
             if barycentric_screen.x >= 0.0 && barycentric_screen.y >= 0.0 && barycentric_screen.z >= 0.0 {
-                let mut z: f32 = barycentric_screen.x * v0.z + barycentric_screen.y * v1.z + barycentric_screen.z * v2.z;
+                let z: f32 = barycentric_screen.x * v0.z + barycentric_screen.y * v1.z + barycentric_screen.z * v2.z;
                 if zbuffer[x as usize + width * y as usize] < z {
                     zbuffer[x as usize + width * y as usize] = z;
                     canvas.set(x as usize, y as usize, color);
@@ -92,7 +92,22 @@ fn draw_triangle(v0: Vector3<f32>, v1: Vector3<f32>, v2: Vector3<f32>, color: &C
     }
 }
 
-fn render_model(shader: &Shader, model: &Vec<[Vertex; 3]>, width: usize, height: usize, mut canvas: &mut Canvas, mut zbuffer: &mut Vec<f32>) {
+fn load_model<R: std::io::BufRead>(r: R) -> Result<Vec<[Vertex; 3]>, ObjError> {
+    let model_obj: Obj = load_obj(r)?;
+
+    let mut model : Vec<[Vertex; 3]>= Vec::new();
+    for indices in model_obj.indices.chunks(3) {
+        let first = model_obj.vertices[indices[0] as usize];
+        let second = model_obj.vertices[indices[1] as usize];
+        let third = model_obj.vertices[indices[2] as usize];
+        model.push([ Vertex{ v: Vector3::new(first.position[0], first.position[1], first.position[2]), n:  Vector3::new(first.normal[0], first.normal[1], first.normal[2]) },
+                            Vertex{ v: Vector3::new(second.position[0], second.position[1], second.position[2]), n: Vector3::new(second.normal[0], second.normal[1], second.normal[2]) },
+                            Vertex{ v: Vector3::new(third.position[0], third.position[1], third.position[2]), n: Vector3::new(third.normal[0], third.normal[1], third.normal[2]) }]);
+    }
+	Ok(model)
+}
+
+fn render_model(shader: &Shader, model: &[[Vertex; 3]], width: usize, height: usize, mut canvas: &mut Canvas, mut zbuffer: &mut Vec<f32>) {
     let c = &Color {r: 255, g: 255, b: 255, a: 255 };
     let c_lines = &Color {r: 0, g: 0, b: 255, a: 255 };
     let mut triangle_count: i32 = 0;
@@ -115,7 +130,7 @@ fn render_model(shader: &Shader, model: &Vec<[Vertex; 3]>, width: usize, height:
 
         if intensity > 0.0 {
             let c_intensity = &Color { r: (c.r as f32 * intensity) as u8, g: (c.g as f32 * intensity) as u8, b: (c.b as f32 * intensity) as u8, a: c.a};
-            draw_triangle(p0, p1, p2, &c_intensity, &mut canvas, &mut zbuffer, width, height);
+            draw_triangle(shader, p0, p1, p2, &c_intensity, &mut canvas, &mut zbuffer, width, height);
         }
     }
     println!("triangle_count: {}", triangle_count)
@@ -125,24 +140,12 @@ fn main() -> Result<(), ObjError> {
     let width: usize = 800;
     let height: usize = 800;
     let mut canvas = Canvas::new(width, height, Color{r: 0, g:0, b: 0, a: 255});
-    let mut zbuffer: Vec<f32> = vec![0.0; width * height];
+    let mut zbuffer: &mut Vec<f32> = &mut vec![0.0; width * height];
     let window: Window = Window::new(&canvas);
     let shader = BasicShader;
 
     let input = BufReader::new(File::open("/Users/bjornmartens/projects/tempfromgithub/tinyrenderer/obj/african_head/african_head.obj")?);
-    let model_obj: Obj = load_obj(input)?;
-
-    let model : &mut Vec<[Vertex; 3]>= &mut Vec::new();
-    for indices in model_obj.indices.chunks(3) {
-        let first = model_obj.vertices[indices[0] as usize];
-        let second = model_obj.vertices[indices[1] as usize];
-        let third = model_obj.vertices[indices[2] as usize];
-        model.push([ Vertex{ v: Vector3::new(first.position[0], first.position[1], first.position[2]), n:  Vector3::new(first.normal[0], first.normal[1], first.normal[2]) },
-                            Vertex{ v: Vector3::new(second.position[0], second.position[1], second.position[2]), n: Vector3::new(second.normal[0], second.normal[1], second.normal[2]) },
-                            Vertex{ v: Vector3::new(third.position[0], third.position[1], third.position[2]), n: Vector3::new(third.normal[0], third.normal[1], third.normal[2]) }]);
-    }
-    let now = Instant::now();
-
+	let model = load_model(input)?;
     let mut previous_time = Instant::now();
     while window.pump() {
         render_model(&shader, &model, width, height, &mut canvas, &mut zbuffer);
