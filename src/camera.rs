1412f7a5237fe97a2449ebec44c810f71e7b94b9
Bjorn Martens <bjorn@expeditious.nl>
2020-04-13 23:04:54 +0200

    add comments for adding roll/yaw/pitch

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,36 @@
 use nalgebra_glm::*;
+/*void Camera::Move(float forward, float left) {
+glm::vec3 direction = CalculateDirectionToMoveIn(around, updown);
+glm::vec3 leftdirection(-direction.z, direction.y, direction.x);
+position += direction * -forward;
+position += leftdirection * left;
+}
+void Camera::MouseMove(float around_diff, float updown_diff) {
+updown += updown_diff;
+around += around_diff;
+updown = std::min(pi_2 - 0.1f, std::max(-pi_2 + 0.1f, updown));
+}
+const glm::mat4 Camera::GetView() {
+glm::mat4 arcBallRotation = ArcBallRotation(around, updown);
+glm::vec3 directiontopos(glm::vec4(0.0f, 0.0f, -1.0f, 1.0f) * arcBallRotation);
+glm::vec3 upvector(glm::vec4(0.0f, 1.0f, 0.0f, 1.0f) * arcBallRotation);
+return glm::lookAt((glm::normalize(directiontopos) * glm::vec3(zoom, zoom, zoom)) + position, position, glm::normalize(upvector));
+}
+glm::vec3 Camera::CalculateDirectionToMoveIn(float around, float updown) {
+glm::vec4 direction = glm::vec4(0.0f, 0.0f, -1.0f, 1.0f) * ArcBallRotation(around, updown);
+direction.y = 0.0f; // remove y component, we are only interested in plane movement
+return glm::normalize(glm::vec3(direction));
+}
+glm::mat4 Camera::ArcBallRotation(float around, float updown) {
+return glm::rotate(glm::mat4(1.0f), updown, glm::vec3(1.0f, 0.0f, 0.0f)) *
+glm::rotate(glm::mat4(1.0f), around, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+glm::vec3 position = glm::vec3(0.0f, 5.0f, 0.0f);
+float updown = 0.0f;
+float around = 0.0f;
+float zoom = 20.0f;
+*/
 
 pub struct Camera {
     initial_up: Vec3,
@@ -15,6 +47,28 @@ impl Camera {
             position: vec3(0.0, 0.0, 2.0),
             orientation: quat_identity(),
         }
+    }
+
+    // rotate around right vector (cross product between up and direction
+    pub fn pitch(&mut self, val: f32) {
+        // calculate up and direction see below
+        // calculate right by cross product of up and direction
+        // use quat_angle_axis
+        // store new orientation by doing orientation * rot
+    }
+
+    // rotate around up vector
+    pub fn yaw(&mut self, val: f32) {
+        // calculate up via orientation, intitial_up
+        // use quat_angle_axis()
+        // store new orientation by doing orientation * rot
+    }
+
+    // rotate around direction vector
+    pub fn roll(&mut self, val: f32) {
+        // calculate direction via orientation, intitial_direction
+        // use quat_angle_axis()
+        // store new orientation by doing orientation * rot
     }
 
     pub fn get_view(&self) -> Mat4 {
