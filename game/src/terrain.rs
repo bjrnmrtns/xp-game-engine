b651064cc755c49de4cdd3517fe45eef89129c71
Bjorn Martens <bjorn@expeditious.nl>
2020-07-03 11:21:24 +0200

    lousy implementation of tile numbering based on lowest lod

--- game/src/terrain.rs
+++ game/src/terrain.rs
@@ -5,13 +5,36 @@ struct TileGenerator {
     lod_size: i32,
 }
 
+fn to_index_of_lod(index: i32, lod: u32) -> i32 {
+    assert!(lod > 0);
+   /* if index < 0 {
+        return  (-1 * (-(index + 1) / (lod + 1)) - 1;
+        //return -1 + (-index / (lod + 1)) * -1;
+    } else {
+        return index / (lod + 1)
+    }*/
+
+    let neg = index < 0;
+    let index = if neg {
+        (index * -1) - 1
+    } else {
+        index
+    };
+    let pos_value_mapped = index / 2_i32.pow(lod);
+    if neg {
+        return (pos_value_mapped + 1) * -1;
+    } else {
+        return pos_value_mapped;
+    }
+}
+
 impl TileGenerator {
     pub fn new() -> Self {
         Self {
             lod_0_grid_pitch: 1, // fixed point where 1 means 10 cm
             lod_levels: 2,
             resolution: 64, // amount of grid points in widht/height of a tile
-            lod_size: 3, // the amount of tiles in width/height of a lod
+            lod_size: 2, // the amount of tiles in width/height of a lod
         }
     }
     pub fn create(&self, lod_level: i32) -> Vec<[i32; 3]> {
@@ -25,22 +48,13 @@ impl TileGenerator {
         }
         grid
     }
-    pub fn within_tile_of_lod(&self, lod_level: i32, tile_position: [i32; 3]) -> Option<[i32; 3]>{
-        let lod_diff = lod_level - tile_position[1];
-        if lod_diff < 0 {
-            None
-        } else if lod_diff == 0 {
-            Some(tile_position)
-        } else {
-            Some([tile_position[0] / self.lod_size, lod_level, tile_position[2] / self.lod_size])
-        }
-    }
     pub fn which_tiles(&self, tile_position: [i32; 3]) -> Vec<[i32; 3]> {
         let mut tiles = Vec::new();
         tiles
     }
 }
 
+//#[test]
 fn test_which_tiles() {
     let tiles = TileGenerator::new().which_tiles([0, 0, 0]);
     assert_eq!(tiles.len(), 9);
@@ -55,11 +69,52 @@ fn test_which_tiles() {
     assert!(tiles.contains(&[1, 0, 1]));
 }
 
-#[test]
+/*#[test]
 fn test_within_tile_of_lod() {
     let tile_gen = TileGenerator::new();
     assert_eq!(tile_gen.within_tile_of_lod(2, [0, 3, 0]), None);
     assert_eq!(tile_gen.within_tile_of_lod(0, [0, 0, 0]), Some([0, 0, 0]));
+    assert_eq!(tile_gen.within_tile_of_lod(1, [1, 0, 0]), Some([0, 1, 0]));
+    assert_eq!(tile_gen.within_tile_of_lod(1, [2, 0, 0]), Some([1, 1, 0]));
+    assert_eq!(tile_gen.within_tile_of_lod(1, [-1, 0, 0]), Some([-1, 1, 0]));
+    assert_eq!(tile_gen.within_tile_of_lod(2, [-4, 0, 0]), Some([-1, 2, 0]));
+    assert_eq!(tile_gen.within_tile_of_lod(2, [-5, 0, 0]), Some([-2, 2, 0]));
+}
+*/
+
+#[test]
+fn test_to_index_of_lod1() {
+    assert_eq!(to_index_of_lod(0, 1), 0);
+    assert_eq!(to_index_of_lod(1, 1), 0);
+    assert_eq!(to_index_of_lod(2, 1), 1);
+    assert_eq!(to_index_of_lod(3, 1), 1);
+    assert_eq!(to_index_of_lod(4, 1), 2);
+    assert_eq!(to_index_of_lod(5, 1), 2);
+    assert_eq!(to_index_of_lod(6, 1), 3);
+    assert_eq!(to_index_of_lod(-3, 1), -2);
+    assert_eq!(to_index_of_lod(-2, 1), -1);
+    assert_eq!(to_index_of_lod(-1, 1), -1);
+
 }
 
+#[test]
+fn test_to_index_of_lod2() {
+    assert_eq!(to_index_of_lod(0, 2), 0);
+    assert_eq!(to_index_of_lod(1, 2), 0);
+    assert_eq!(to_index_of_lod(2, 2), 0);
+    assert_eq!(to_index_of_lod(3, 2), 0);
+    assert_eq!(to_index_of_lod(4, 2), 1);
+    assert_eq!(to_index_of_lod(-5, 2), -2);
+    assert_eq!(to_index_of_lod(-4, 2), -1);
+    assert_eq!(to_index_of_lod(-3, 2), -1);
+    assert_eq!(to_index_of_lod(-2, 2), -1);
+    assert_eq!(to_index_of_lod(-1, 2), -1);
+}
 
+#[test]
+fn test_to_index_of_lod3() {
+    assert_eq!(to_index_of_lod(7, 3), 0);
+    assert_eq!(to_index_of_lod(8, 3), 1);
+    assert_eq!(to_index_of_lod(-8, 3), -1);
+    assert_eq!(to_index_of_lod(-9, 3), -2);
+}
