ee4c2df571ea8ea52fb63e1c603183edebd5d93d
Bjorn Martens <bjorn@expeditious.nl>
2020-07-03 16:49:03 +0200

    little refactoring of grid generation

--- game/src/terrain.rs
+++ game/src/terrain.rs
@@ -1,8 +1,43 @@
-struct TileGenerator {
-    lod_0_grid_pitch: i32,
-    resolution: i32,
-    lod_levels: i32,
-    lod_size: i32,
+const TILE_SIZE: usize = 64;
+
+#[derive(Clone)]
+pub struct Element {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Element {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
+        Self {
+            x,
+            y,
+            z,
+        }
+    }
+}
+
+pub struct Tile {
+    x: i32,
+    z: i32,
+    elements: Vec<Element>,
+}
+
+impl Tile {
+    pub fn new() -> Self {
+        let mut elements = vec!(Element::new(0, 0, 0); (TILE_SIZE + 1) * (TILE_SIZE + 1));
+        let pitch = 1;
+        for z in 0..TILE_SIZE + 1 {
+            for x in 0..TILE_SIZE + 1 {
+                elements[z * (TILE_SIZE + 1) + x] = Element::new(x as i32 * pitch, 0, z as i32 * pitch);
+            }
+        }
+        Self {
+            x: 0,
+            z: 0,
+            elements,
+        }
+    }
 }
 
 fn to_index_of_lod(index: i32, lod: u32, lod_size: i32) -> i32 {
@@ -14,51 +49,30 @@ fn to_index_of_lod(index: i32, lod: u32, lod_size: i32) -> i32 {
     }
 }
 
-impl TileGenerator {
-    pub fn new() -> Self {
-        Self {
-            lod_0_grid_pitch: 1, // fixed point where 1 means 10 cm
-            lod_levels: 2,
-            resolution: 64, // amount of grid points in widht/height of a tile
-            lod_size: 2, // the amount of tiles in width/height of a lod
-        }
+fn tiles_around_tile(tile: &[i32; 3]) -> [[i32; 3]; 9] {
+     [[tile[0] - 1, tile[1], tile[2] - 1],
+      [tile[0] - 1, tile[1], tile[2]],
+      [tile[0] - 1, tile[1], tile[2] + 1],
+      [tile[0], tile[1], tile[2] - 1],
+      [tile[0], tile[1], tile[2]],
+      [tile[0], tile[1], tile[2] + 1],
+      [tile[0] + 1, tile[1], tile[2] - 1],
+      [tile[0] + 1, tile[1], tile[2]],
+      [tile[0] + 1, tile[1], tile[2] + 1]]
+}
+pub fn which_tiles(tile: [i32; 3], max_lod: u32) -> Vec<[i32; 3]> {
+    assert_eq!(tile[1], 0);
+    let mut tiles = Vec::new();
+    for lod in 0..max_lod + 1 {
+        let lod_tile = [to_index_of_lod(tile[0], lod, 2), lod as i32, to_index_of_lod(tile[2], lod, 2)];
+        tiles.extend(tiles_around_tile(&lod_tile).iter());
     }
-    pub fn create(&self, lod_level: i32) -> Vec<[i32; 3]> {
-        let grid_pitch = self.lod_0_grid_pitch * (lod_level + 1);
-        let grid_size = grid_pitch * self.resolution;
-        let mut grid = Vec::new();
-        for z in 0..grid_size {
-            for x in 0..grid_size {
-                grid.push([x * grid_pitch, 0, z * grid_pitch]);
-            }
-        }
-        grid
-    }
-    fn tiles_around_tile(&self, tile: &[i32; 3]) -> [[i32; 3]; 9] {
-         [[tile[0] - 1, tile[1], tile[2] - 1],
-          [tile[0] - 1, tile[1], tile[2]],
-          [tile[0] - 1, tile[1], tile[2] + 1],
-          [tile[0], tile[1], tile[2] - 1],
-          [tile[0], tile[1], tile[2]],
-          [tile[0], tile[1], tile[2] + 1],
-          [tile[0] + 1, tile[1], tile[2] - 1],
-          [tile[0] + 1, tile[1], tile[2]],
-          [tile[0] + 1, tile[1], tile[2] + 1]]
-    }
-    pub fn which_tiles(&self, tile: [i32; 3], max_lod: u32) -> Vec<[i32; 3]> {
-        assert_eq!(tile[1], 0);
-        let mut tiles = Vec::new();
-        for lod in 0..max_lod + 1 {
-            let lod_tile = [to_index_of_lod(tile[0], lod, 2), lod as i32, to_index_of_lod(tile[2], lod, 2)];
-            tiles.extend(self.tiles_around_tile(&lod_tile).iter());
-        }
-        tiles
-    }
+    tiles
 }
 
 #[test]
 fn test_which_tiles_1() {
-    let tiles = TileGenerator::new().which_tiles([0, 0, 0], 0);
+    let tiles = which_tiles([0, 0, 0], 0);
     assert_eq!(tiles.len(), 9);
     assert!(tiles.contains(&[-1, 0, -1]));
     assert!(tiles.contains(&[0, 0, -1]));
@@ -73,7 +87,7 @@ fn test_which_tiles_1() {
 
 #[test]
 fn test_which_tiles_2() {
-    let tiles = TileGenerator::new().which_tiles([-7, 0, 5], 2);
+    let tiles = which_tiles([-7, 0, 5], 2);
     assert_eq!(tiles.len(), 27);
     assert!(tiles.contains(&[-8, 0, 4]));
     assert!(tiles.contains(&[-7, 0, 4]));
