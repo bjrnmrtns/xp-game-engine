1ec9c2c53150dd4a862497ccc4e5a9d8fb8d8fb9
Bjorn Martens <bjorn@expeditious.nl>
2021-01-29 10:33:38 +0100

    create window

--- low_poly_nice_graphics/src/main.rs
+++ low_poly_nice_graphics/src/main.rs
@@ -1,1 +1,35 @@
-fn main() {}
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .expect("Could not create window");
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = winit::event_loop::ControlFlow::Poll;
+        match event {
+            Event::RedrawRequested(_) => (), // run loop
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => match event {
+                WindowEvent::Resized(physical_size) => {
+                    //futures::executor::block_on(renderer.resize(*physical_size));
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    //futures::executor::block_on(renderer.resize(**new_inner_size));
+                }
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            _ => (),
+        }
+    });
+}
