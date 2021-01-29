f89b4050d60dfaf9e5c8bf2e042cfc4c6eff3fd2
Bjorn Martens <bjorn@expeditious.nl>
2021-01-29 17:32:44 +0100

    add simple lighting to triangle

--- low_poly_nice_graphics/src/renderer/pipeline.rs
+++ low_poly_nice_graphics/src/renderer/pipeline.rs
@@ -71,17 +71,17 @@ impl Default for Asset {
             vertices: vec![
                 Vertex {
                     position: vec3(0.5, -0.5, 0.0),
-                    normal: vec3(0.0, 0.5, 0.0),
+                    normal: vec3(0.0, 0.0, 1.0),
                     color: vec3(1.0, 0.0, 0.0),
                 },
                 Vertex {
                     position: vec3(0.0, 0.5, 0.0),
-                    normal: vec3(0.5, -0.5, 0.0),
+                    normal: vec3(0.0, 0.0, 1.0),
                     color: vec3(1.0, 0.0, 0.0),
                 },
                 Vertex {
                     position: vec3(-0.5, -0.5, 0.0),
-                    normal: vec3(0.5, -0.5, 0.0),
+                    normal: vec3(0.0, 0.0, 1.0),
                     color: vec3(1.0, 0.0, 0.0),
                 },
             ],
